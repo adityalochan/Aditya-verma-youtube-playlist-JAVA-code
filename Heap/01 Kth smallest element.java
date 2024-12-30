95c7640516ddec0a176bffefc1bb1327ba74e430
adityalochan <aditya.lochan25@gmail.com>
2024-12-30 14:35:03 -0500

    added modules

---
+++ Heap/01 Kth smallest element.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+public class Solution {
+    public int findKthSmallest(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : nums) {
+            maxHeap.add(num);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        return maxHeap.peek();
+    }
+}
+
