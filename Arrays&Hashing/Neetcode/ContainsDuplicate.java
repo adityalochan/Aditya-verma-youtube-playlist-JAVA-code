a75e2c0ae24f7d5ae7a531cf3402504fe06a832b
adityalochan <aditya.lochan25@gmail.com>
2025-12-02 17:10:33 -0400

    contains duplicate

--- Arrays&Hashing/Neetcode/ContainsDuplicate.java
+++ Arrays&Hashing/Neetcode/ContainsDuplicate.java
@@ -1,1 +1,32 @@
-BRUTEFORCE
+/*BRUTEFORCE*/
+/*T: O(N*N) | T:O()*/
+public boolean containsDuplicate(int[] nums) {
+    for(int i=0;i<nums.length;i++){
+        int a=nums[i];
+        for(int j=i+1;j<nums.length;j++){
+            int b=nums[j];
+            if(a==b) return true;;
+        }
+    }
+    return false;
+}
+
+/*SORTING*/
+public boolean containsDuplicate(int[] nums) {
+    Arrays.sort(nums);
+    for(int i=0;i<nums.length-1;i++){
+        if(nums[i]==nums[i+1])return true;
+    }
+    return false;
+}
+
+/*HASHSET*/
+/*T: O() | T:O()*/
+public boolean containsDuplicate(int[] nums) {
+    Set<Integer> hs = new HashSet<>();
+    for(int num:nums){
+        if(hs.contains(num))return true;
+        else hs.add(num);
+    }
+    return false;
+}
