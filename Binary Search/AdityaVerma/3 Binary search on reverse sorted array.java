5fe0b244cecd7c59ad8d1fb6bcef094aeda2685e
adityalochan <aditya.lochan25@gmail.com>
2025-12-05 10:19:12 -0400

    added new logic

--- Binary Search/AdityaVerma/3 Binary search on reverse sorted array.java
+++ Binary Search/AdityaVerma/3 Binary search on reverse sorted array.java
@@ -6,4 +6,21 @@ Same a linear search
 ------------------
 BINARY SEARCH
 ------------------
-Same as basic binary search
+/*Opposite of sorted.*/
+/*T:O(log N) | S:O(1)*/
+public int search(int[] nums, int target) {
+    int start = 0;
+    int end = nums.length - 1;
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (target == nums[mid]) {
+            return mid;
+        } else if (target < nums[mid]) {
+            start = mid + 1;   // reversed comparison
+        } else {
+            end = mid - 1;
+        }
+        return -1;
+    }
+}
