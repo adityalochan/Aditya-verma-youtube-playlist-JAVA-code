5a1763cf75e9a97ab1dacba8b6833659a8a6ef41
adityalochan <aditya.lochan25@gmail.com>
2025-12-28 23:57:44 -0400

    added solution next smaller to right

--- Stack/AdityaVerma/4. NSR | Nearest Smaller to Right.java
+++ Stack/AdityaVerma/4. NSR | Nearest Smaller to Right.java
@@ -1,7 +1,39 @@
+/*Given an array of integers, find the closest (not considering distance, but value)
+smaller on rightof every element. If an element has no smaller on the right side, print -1.*/
 ------------------
 BRUTEFORCE
 ------------------
-
+public int[] nearestSmallerToRight(int[] arr, int n) {
+    int[] result = new int[n];
+    for(int i = 0; i < n; i++){
+        result[i] = -1;
+        for(int j = i+1; j < n; j++){
+            if(arr[j] < arr[i]){
+                result[i] = arr[j];
+                break;
+            }
+        }
+    }
+    return result;
+}
 ------------------
 STACK
 ------------------
+public int[] nearestSmallerToRight(int[] arr, int n) {
+    int[] result = new int[n];
+    Stack<Integer> stack = new Stack<>();
+    for(int i=n-1;i>=0;i++){
+        if(stack.isEmpty())
+            result[i]=-1;
+        else if(stack.peek()<arr[i])
+            result[i]=stack.peek();
+        else{
+            while(!stack.isEmpty() && stack.peek()>=arr[i]){
+                stack.pop();
+            }
+            result[i]=stack.isEmpty()? -1:stack.peek();
+        }
+        stack.push(arr[i]);
+    }
+    return result;
+}
