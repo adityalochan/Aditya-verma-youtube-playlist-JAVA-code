91fe05de4a20d9e0ac3e9e4fd620587d826bd3ee
adityalochan <aditya.lochan25@gmail.com>
2026-01-05 15:53:47 -0400

    edited solution MAH

--- Stack/AdityaVerma/6. MAH | Maximum Area of Histogram.java
+++ Stack/AdityaVerma/6. MAH | Maximum Area of Histogram.java
@@ -13,25 +13,24 @@ Eg
 ------------------
 BRUTEFORCE
 ------------------
-public long getMaxArea(long[] arr, int n) {
-    long maxArea = 0;
+public int largestRectangleArea(int[] heights) {
+    int n = heights.length;
+    int maxArea = 0;
 
     for (int i = 0; i < n; i++) {
-        long height = arr[i];
+        int height = heights[i];
         int left = i;
         int right = i;
 
         // Expand to the left
-        while (left >= 0 && arr[left] >= height) {
+        while (left >= 0 && heights[left] >= height)
             left--;
-        }
         // Expand to the right
-        while (right < n && arr[right] >= height) {
+        while (right < n && heights[right] >= height)
             right++;
-        }
-        long width = right - left - 1;
-        long area = height * width;
 
+        int width = right - left - 1;
+        int area = height * width;
         maxArea = Math.max(maxArea, area);
     }
     return maxArea;
@@ -40,56 +39,55 @@ public long getMaxArea(long[] arr, int n) {
 ------------------
 STACK
 ------------------
-import java.util.*;
-
-
-public long getMaxArea(long[] arr, int n) {
-
+public int largestRectangleArea(int[] heights) {
+    int n = heights.length;
     int[] left = new int[n];   // NSL indices
     int[] right = new int[n];  // NSR indices
-    Stack<long[]> stack1 = new Stack<>(); // {height, index}
-    Stack<long[]> stack2 = new Stack<>();
+    Stack<int[]> stack1 = new Stack<>(); // {height, index}
+    Stack<int[]> stack2 = new Stack<>();
     int pseudoLeft = -1;
     int pseudoRight = n;
 
-    // -------- NSL: Nearest Smaller to Left --------
+    // -------- NSL (Nearest Smaller to Left) --------
     for (int i = 0; i < n; i++) {
-        if (stack1.isEmpty())
+
+        if (stack1.isEmpty()) {
             left[i] = pseudoLeft;
-        else if (stack1.peek()[0] < arr[i]) {
-            left[i] = (int) stack1.peek()[1];
+        }
+        else if (stack1.peek()[0] < heights[i]) {
+            left[i] = stack1.peek()[1];
         }
         else {
-            while (!stack1.isEmpty() && stack1.peek()[0] >= arr[i]) {
+            while (!stack1.isEmpty() && stack1.peek()[0] >= heights[i]) {
                 stack1.pop();
             }
-            left[i] = stack1.isEmpty() ? pseudoLeft : (int) stack1.peek()[1];
+            left[i] = stack1.isEmpty() ? pseudoLeft : stack1.peek()[1];
         }
-        stack1.push(new long[]{arr[i], i});
+        stack1.push(new int[]{heights[i], i});
     }
-    // --------NSR: Nearest Smaller to Right --------
+
+    // -------- NSR (Nearest Smaller to Right) --------
     for (int i = n - 1; i >= 0; i--) {
         if (stack2.isEmpty()) {
             right[i] = pseudoRight;
         }
-        else if (stack2.peek()[0] < arr[i]) {
-            right[i] = (int) stack2.peek()[1];
+        else if (stack2.peek()[0] < heights[i]) {
+            right[i] = stack2.peek()[1];
         }
         else {
-            while (!stack2.isEmpty() && stack2.peek()[0] >= arr[i]) {
+            while (!stack2.isEmpty() && stack2.peek()[0] >= heights[i]) {
                 stack2.pop();
             }
-            right[i] = stack2.isEmpty() ? pseudoRight : (int) stack2.peek()[1];
+            right[i] = stack2.isEmpty() ? pseudoRight : stack2.peek()[1];
         }
-        stack2.push(new long[]{arr[i], i});
+
+        stack2.push(new int[]{heights[i], i});
     }
-
     // -------- Calculate Maximum Area --------
-    long maxArea = Long.MIN_VALUE;
-
+    int maxArea = 0;
     for (int i = 0; i < n; i++) {
-        long width = right[i] - left[i] - 1;
-        long area = width * arr[i];
+        int width = right[i] - left[i] - 1;
+        int area = width * heights[i];
         maxArea = Math.max(maxArea, area);
     }
     return maxArea;
